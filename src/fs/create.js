0c8f600d61472464a3ddd1b7720e27f7a501ee91
bp26 <aleshak260006@gmail.com>
2023-06-30 12:10:56 +0000

    feat: complete create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+const data = 'I am fresh and young';
+
+const checkFile = async (filePath) => {
+  try {
+    await fs.access(filePath, fs.constants.F_OK);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
 const create = async () => {
-    // Write your code here 
+  const fileExists = await checkFile(filePath);
+  if (fileExists) {
+    throw new Error('FS operation failed');
+  } else {
+    await fs.writeFile(filePath, data);
+  }
 };
 
 await create();
