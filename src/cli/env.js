bd0ad9bc625debbf3bb44dff5c3d286a1b454307
bp26 <aleshak260006@gmail.com>
2023-07-04 11:27:25 +0000

    feat: complete cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envArr = Object.entries(process.env);
+  envArr.forEach((env) => {
+    if (env[0].startsWith('RSS_')) {
+      console.log(`${env[0]}=${env[1]}`);
+    }
+  });
 };
 
 parseEnv();
