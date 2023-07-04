e6c24dffb1122d33bedb647040692170edb58d72
bp26 <aleshak260006@gmail.com>
2023-07-04 11:15:25 +0000

    feat: complete fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filepath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const data = await fs.readFile(filepath);
+    console.log(data.toString());
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
