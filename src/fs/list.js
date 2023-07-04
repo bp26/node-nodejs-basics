ff7ba36a0f5122fd166af00a60150fd5304333d1
bp26 <aleshak260006@gmail.com>
2023-07-04 11:12:27 +0000

    feat: complete fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const dirpath = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir(dirpath);
+    for (const file of files) {
+      console.log(file);
+    }
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
