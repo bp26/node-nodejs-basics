840c0ad567b887a8d212cc11b77d897861829abf
bp26 <aleshak260006@gmail.com>
2023-07-04 12:57:52 +0000

    feat: complete hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import crypto from 'crypto';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filepath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = await fs.readFile(filepath);
+  const hash = crypto.createHash('sha256');
+  hash.update(file);
+  const hex = hash.digest('hex');
+  console.log(hex);
 };
 
 await calculateHash();
