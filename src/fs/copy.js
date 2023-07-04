dd0e78b7d0340e2f57af2cfe753bf7526140c7da
bp26 <aleshak260006@gmail.com>
2023-07-04 08:50:29 +0000

    feat: complete copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
-const copy = async () => {
-    // Write your code here 
-};
+import { fstat } from 'fs';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const dir = path.join(__dirname, 'files');
+const newDir = path.join(__dirname, 'files_copy');
+
+const copy = async () => {
+  try {
+    await fs.mkdir(newDir);
+    const files = await fs.readdir(dir);
+    for (const file of files) {
+      const filepath = path.join(dir, file);
+      const newFilepath = path.join(newDir, file)
+      await fs.copyFile(filepath, newFilepath);
+    }
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
+};
 await copy();
