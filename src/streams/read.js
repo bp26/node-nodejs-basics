c8fa81ea01155e6abe2e85ea220cd056e7b43e13
bp26 <aleshak260006@gmail.com>
2023-07-05 08:31:51 +0000

    feat: complete streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filepath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  pipeline(createReadStream(filepath), stdout, (err) => {
+    console.log('error');
+  });
 };
 
 await read();
