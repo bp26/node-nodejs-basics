4a472a9c7786ddd14b77c9b553e60a150d971cfe
bp26 <aleshak260006@gmail.com>
2023-07-05 11:21:47 +0000

    feat: complete zip:decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import zlib from 'zlib';
+import { createWriteStream, createReadStream } from 'fs';
+import { pipeline } from 'stream';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filepath = path.join(__dirname, 'files', 'archive.gz');
+const tranformedFilepath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+  pipeline(
+    createReadStream(filepath),
+    zlib.createUnzip(),
+    createWriteStream(tranformedFilepath),
+    (err) => {
+      if (err) {
+        console.log(err.stack);
+      }
+    }
+  );
 };
 
 await decompress();
