9ab483926227375f2d4abb1454bc697398c9b5ae
bp26 <aleshak260006@gmail.com>
2023-07-05 08:36:59 +0000

    feat: complete streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { stdin } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filepath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  pipeline(stdin, createWriteStream(filepath), (err) => {
+    console.log(err.stack);
+  });
 };
 
 await write();
