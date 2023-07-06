f489429d8648c24e8378ee41c4ba1e068771818e
bp26 <aleshak260006@gmail.com>
2023-07-06 12:20:46 +0000

    feat: partially complete cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { fork } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const childPath = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork(childPath, [...args], { stdio: 'pipe' });
+  pipeline(process.stdin, child.stdin, (err) => {
+    if (err) {
+      console.log(err.stack);
+    }
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['1', '2']);
