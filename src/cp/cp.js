68ca87f4c3d16c99eb774e7ba25c81cf65963664
bp26 <aleshak260006@gmail.com>
2023-07-11 11:24:17 +0000

    feat: complete cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,7 +1,6 @@
 import { fork } from 'child_process';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { pipeline } from 'stream';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,11 +9,6 @@ const childPath = path.join(__dirname, 'files', 'script.js');
 
 const spawnChildProcess = async (args) => {
   const child = fork(childPath, [...args], { stdio: 'pipe' });
-  pipeline(process.stdin, child.stdin, (err) => {
-    if (err) {
-      console.log(err.stack);
-    }
-  });
 };
 
 spawnChildProcess(['1', '2']);
